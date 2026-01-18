a1de703d4da0f15f1308ee6563a2b0f4a901b576
Salih Alcı <salihalci@gmail.com>
2026-01-18 16:54:14 +0300

    dynamic dropdown eklendi.

--- thymleaf-demo/src/main/java/com/sa/demothymleaf/controller/StudentController.java
+++ thymleaf-demo/src/main/java/com/sa/demothymleaf/controller/StudentController.java
@@ -1,14 +1,23 @@
 package com.sa.demothymleaf.controller;
 
 import com.sa.demothymleaf.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    //inject from propeties.file
+
+    @Value("${countries}")
+    private List<String> countries;
+
     @GetMapping("/show-student-form")
     public String showForm(Model theModel) {
         Student theStudent = new Student();
@@ -16,11 +25,27 @@ public class StudentController {
         return "student-form";
     }
 
+
+
+
     @GetMapping("/show-student-form-dropdown")
     public String showFormDropdown(Model theModel) {
         Student theStudent = new Student();
         theModel.addAttribute("student", theStudent);
         return "student-form-dropdown";
+    }
+
+
+
+    @GetMapping("/show-student-form-dropdown2")
+    public String showFormDropdown2(Model theModel) {
+        Student theStudent = new Student();
+
+        theModel.addAttribute("student", theStudent);
+
+        theModel.addAttribute("countries", countries);
+
+        return "student-form-dropdown2";
     }
 
     @PostMapping("/process-student-form")
