95aebf16b13e879d351d92debae680f37c368201
Salih Alcı <salihalci@gmail.com>
2026-01-18 15:40:37 +0300

    Form databinding for student

---
+++ thymleaf-demo/src/main/java/com/sa/demothymleaf/controller/StudentController.java
@@ -0,0 +1,25 @@
+package com.sa.demothymleaf.controller;
+
+import com.sa.demothymleaf.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+    @GetMapping("/show-student-form")
+    public String showForm(Model theModel) {
+        Student theStudent = new Student();
+        theModel.addAttribute("student", theStudent);
+        return "student-form";
+    }
+
+    @PostMapping("/process-student-form")
+    public String processStudentForm(@ModelAttribute("student") Student theStudent, Model theModel) {
+        System.out.println(theStudent.getFirstName() + " " + theStudent.getLastName());
+        theModel.addAttribute("student", theStudent);
+        return "student-confirmation";
+    }
+}
