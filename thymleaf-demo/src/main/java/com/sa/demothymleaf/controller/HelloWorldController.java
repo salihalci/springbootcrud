34b625d32fd47766c8b5af8f48b7ad7aa0ba1f0a
Salih Alcı <salihalci@gmail.com>
2026-01-18 13:58:12 +0300

    new codes added.

---
+++ thymleaf-demo/src/main/java/com/sa/demothymleaf/controller/HelloWorldController.java
@@ -0,0 +1,51 @@
+package com.sa.demothymleaf.controller;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping("show-form")
+    public String showForm(){
+        return "helloworld-form";
+    }
+
+    @RequestMapping("process-form")
+    public String processForm(){
+        return "process-form";
+    }
+
+    @RequestMapping("show-formV2")
+    public String showFormV2(){
+        return "helloworld-formV2";
+    }
+    @RequestMapping("process-formV2")
+    public String processFormV2(HttpServletRequest request, Model model){
+        String theName = request.getParameter("studentName");
+        theName = theName.toUpperCase();
+        String result = "UpperCase! " + theName;
+        model.addAttribute("message", result);
+        return "process-formV2";
+    }
+    @RequestMapping("show-formV3")
+    public String showFormV3(){
+        return "helloworld-formV3";
+    }
+
+
+    //Requestparam ile form üzerindeki alanı otomatik olarak map ediyoruz.
+    @RequestMapping("process-formV3")
+    public String processFormV3(@RequestParam("studentName") String theName, Model model){
+
+        //String theName = request.getParameter("studentName");
+
+        theName = theName.toUpperCase();
+        String result = "UpperCase! " + theName;
+        model.addAttribute("message", result);
+        return "process-formV3";
+    }
+}
